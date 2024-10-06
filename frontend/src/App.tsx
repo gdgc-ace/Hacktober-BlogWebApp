20d707d8a5d7c630e76b159c8a93d1c3474615be
Shivam Narkar <snarkar@chuckads.com>
2024-10-07 01:03:50 +0530

    Added some example stuff 🔥

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import { Outlet } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
     <>
-      <nav className='text-3xl font-bold underline'>Navbar</nav>
+      {/* <nav className='text-3xl font-bold underline'>Navbar</nav> */}
+      <Navbar/>
       <Outlet />
-      <footer>Footer</footer>
+      <Footer/>
     </>
   )
 }
