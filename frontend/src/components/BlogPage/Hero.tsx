73fe1429a84b340fc72024457eaa831ff918b2d6
Shivam Narkar <snarkar@chuckads.com>
2024-10-12 02:13:19 +0530

    Fixed some design of Navbar and font.

--- frontend/src/components/BlogPage/Hero.tsx
+++ frontend/src/components/BlogPage/Hero.tsx
@@ -9,14 +9,14 @@ const Hero = () => {
         />
 
         <div className="relative flex flex-col justify-center items-center h-full text-center">
-          <h1 className=" text-6xl sm:text-8xl md:text-8xl flex flex-col  mb-4 uppercase  text-primary p-5 text-[#FFFFFF] tracking-wider">
-            <span className="inline font-['Anton'] tracking-wider">Stories from the</span>
-            <div className="flex justify-between font-['Anton']">
+          <h1 className=" text-6xl sm:text-8xl md:text-8xl flex flex-col  mb-4 uppercase  text-primary p-5 text-[#FFFFFF] tracking-wide">
+            <span className="inline font-bold tracking-wide">Stories from the</span>
+            <div className="flex justify-evenly font-bold">
               {/* <span className="text-green-500">G</span> */}
               {/* <span className="text-red-500">D</span> */}
               {/* <span className="text-yellow-300">G</span> */}
               {/* <span className="text-blue-400">C</span> */}
-              <p className=" font-['Anton']  px-5 rounded-[50px]">GDSC</p>
+              <p className="text-[#febb00] ">GDGC</p>
               &nbsp;Community
             </div>
           </h1>
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,19 +1,25 @@
+import { useEffect } from "react"
+
 const Navbar = () => {
+  useEffect(()=>{
+
+  },[])
   return (
     <>
       <nav>
-        <div className="flex justify-evenly w-full p-10 fixed z-10 text-white">
+        <div className="flex justify-evenly items-center w-full p-10 fixed z-10 text-white">
           <div className="w-full text-left ml-16 p-1">
-            <p className="text-4xl font-bold">GDGC</p>
-          </div>
-          <div className="flex list-none items-center mx-16 font-semibold text-md w-[30%] justify-around">
+            <img src="./logo-1.png" className="w-[60px]"/>
+            {/* <img src="./logo-2.png" className="w-[260px] "/> */}
+                      </div>
+          <div className="flex list-none items-center mx-16 text-2xl w-[30%] justify-around">
             <li>
                 <a href="">About</a>
             </li>
             <li>
                 <a href="">Blog</a>
             </li>
-            <li className="bg-[#2B8C44] px-4 py-2 rounded-md text-white">
+            <li className="bg-[#2a83fb] px-8 py-1 rounded-md text-white">
                 <a href="">JOIN</a>
             </li>
           </div>
