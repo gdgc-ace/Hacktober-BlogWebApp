ba6d9f09b314bd8066b242f382c2ae00bd451e14
VipulMore11 <vvm3875@gmail.com>
2024-10-30 23:14:43 +0530

    CRUD APIs Implemented

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -1,19 +1,77 @@
-const getBlogs = (req, res) => {
-  res.status(200).json({message : "Get all blogs"});
-}
-const getBlog = (req, res) => {
-  res.status(200).json({message : `blog ${req.params.id}`});
+const Blog = require('../models/blogModel.js')
+
+// Get all blogs
+const getBlogs = async (req, res) => {
+  try {
+    const blogs = await Blog.find()
+    res.status(200).json(blogs)
+  } catch (error) {
+    res.status(500).json({ message: error.message })
   }
-const createBlog = (req, res) => {
-  console.log("Data ", req.body);
-  const data = req.body
-  res.status(201).json({message : `Blog created ${data.nomber}`});
-}
-const updateBlogs = (req, res) => {
-  res.status(200).json({message : `Blog updated ${req.params.id}`});
-}
-const deleteBlogs = (req, res) => {
-  res.status(200).json({message : "Get all blogs"});
 }
 
-module.exports = {getBlogs, getBlog, createBlog, updateBlogs, deleteBlogs}
+// Get a single blog by ID
+const getBlog = async (req, res) => {
+  try {
+    const blog = await Blog.findById(req.params.id)
+    if (!blog) {
+      return res.status(404).json({ message: 'Blog not found' })
+    }
+    res.status(200).json(blog)
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+}
+
+// Create a new blog
+const createBlog = async (req, res) => {
+  const { title, content, images, author, tags, isPublished } = req.body
+  const blog = new Blog({
+    title,
+    content,
+    images,
+    author,
+    tags,
+    isPublished,
+  })
+
+  try {
+    const newBlog = await blog.save()
+    res.status(201).json(newBlog)
+  } catch (error) {
+    res.status(400).json({ message: error.message })
+  }
+}
+
+// Update an existing blog by ID
+const updateBlog = async (req, res) => {
+  try {
+    const updatedBlog = await Blog.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true, runValidators: true }
+    )
+
+    if (!updatedBlog) {
+      return res.status(404).json({ message: 'Blog not found' })
+    }
+    res.status(200).json(updatedBlog)
+  } catch (error) {
+    res.status(400).json({ message: error.message })
+  }
+}
+
+// Delete a blog by ID
+const deleteBlog = async (req, res) => {
+  try {
+    const blog = await Blog.findByIdAndDelete(req.params.id)
+    if (!blog) {
+      return res.status(404).json({ message: 'Blog not found' })
+    }
+    res.status(200).json({ message: 'Blog deleted successfully' })
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+}
+
+module.exports = { getBlogs, getBlog, createBlog, updateBlog, deleteBlog }
--- backend/routes/blogRoutes.js
+++ backend/routes/blogRoutes.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const multer = require("multer");
 const router = express.Router();
-const {getBlogs, getBlog, createBlog, updateBlogs, deleteBlogs} = require("../controllers/blogController")
+const {getBlogs, getBlog, createBlog, updateBlog, deleteBlog} = require("../controllers/blogController")
 const upload = multer();
 
 router.route('/get_all_blogs').get(getBlogs);
 router.route('/get_blog/:id').get(getBlog);
 router.route('/create_blog/').post(upload.none(), createBlog);
-router.route('/update_blog/:id').put(updateBlogs);
-router.route('/delete_blog/:id').delete(deleteBlogs);
+router.route('/update_blog/:id').put(updateBlog);
+router.route('/delete_blog/:id').delete(deleteBlog);
 
 module.exports = router;
