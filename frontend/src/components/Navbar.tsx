f94e675275321f3e2c65dd852e28d1b0ee140c92
insane-io <sanedhruv1604@gmail.com>
2024-10-31 22:30:13 +0530

    Navbar Updated

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,32 +1,45 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
-    <>
-      <nav>
-        <div className="flex justify-evenly items-center w-full p-10 fixed z-10 text-white">
-          <div className="w-full text-left ml-16 p-1">
-            <Link to="/"> 
-            <img src="./logo-1.png" className="w-[60px]"/>
-            </Link>
-            {/* <img src="./logo-2.png" className="w-[260px] "/> */}
-          </div>
-          <div className="flex list-none items-center mx-16 text-2xl w-[30%] justify-around">
-            <li>
-                <a href="" >About</a>
-            </li>
-            <li>
-                <a href="">Blog</a>
-            </li>
-            <li className="bg-[#FEBB00] px-8 py-1 rounded-md text-white">
-                <Link to="/login">JOIN</Link>
-            </li>
-          </div>
+    <nav className={`fixed w-full z-10 p-4 ${isScrolled ? 'bg-gray-800 shadow-lg transition-all' : 'bg-transparent'}`}>
+      <div className="flex justify-evenly items-center text-white">
+        <div className="w-full text-left ml-16 p-1">
+          <Link to="/"> 
+            <img src="./logo-1.png" className="w-[60px]" alt="Logo" />
+          </Link>
         </div>
-      </nav>
-    </>
-  )
-}
+        <div className="flex list-none items-center mx-16 text-2xl w-[30%] justify-around">
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#blog">Blog</a>
+          </li>
+          <li className="bg-[#FEBB00] px-8 py-1 rounded-md text-white">
+            <Link to="/login">JOIN</Link>
+          </li>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
